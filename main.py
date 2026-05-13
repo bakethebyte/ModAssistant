ad576f0742fdbf1c26c1b7d22cca5d4c919e2540
bakethebyte <ulianiki2025@gmail.com>
2026-05-13 22:03:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import praw
+
+# Наш список слів, які бот буде шукати
+BANNED_WORDS = ["слово1", "слово2"] 
+
+def run_bot():
+    print("Бот RedditModAssistant запущений!")
+    # Тут буде підключення до Reddit
+
+if __name__ == "__main__":
+    run_bot()
